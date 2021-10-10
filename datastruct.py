284880fee0674e30f0d7de7a1156cf4a712a1e58
Namitha S Vijayan <namithavijayan24@gmail.com>
2021-10-11 01:00:26 +0530

    Data Structures

---
+++ datastruct.py
@@ -0,0 +1,6 @@
+a={1,2,3,4,0,8}
+b={4,5,3,7,6}
+print("A Union B",a.union(b))
+print("A intersection B",a.intersection(b))
+print("Difference of A and B",a.difference(b))
+print("Symmetric difference of A and B",a.symmetric_difference(b))
